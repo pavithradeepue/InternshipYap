02ebd5d681c9787ed44eb08d60bacf054fc94bf7
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-06-05 12:07:51 +0530

    Add files via upload

---
+++ GipraBusinessSolutions/Week1/myfirstwp.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My PHP Page</title>
+</head>
+<body>
+<p style="text-align: center; font-size: 24px; font-weight: bold; color: #663399;">
+  This is a centered and enlarged text!
+</p>
+
+  <?php
+    echo '<p style="text-align: center; font-size: 22px; font_weight: 500; color: #ffffff;">Hello, World!</p>';
+    echo '<p style="text-align:center; font-size: 20px; color: #ffebcd;">This is Pavithra here</p>';
+  ?>
+<style>
+  body {
+    background-color: #9370DB; /* Medium Purple */
+    color: #fff;
+    font-family: Arial, sans-serif;
+    padding: 20px;
+  }
+</style>
+
+
+</body>
+</html>
